580223526985c763d111cfcf6f0b9e2c25bda3f9
sergi0123456 <sergiterrones021@gmail.com>
2025-03-20 16:06:38 +0100

    Soy un ladron de META

--- LadronPersonaje.java
+++ LadronPersonaje.java
@@ -1,2 +1,30 @@
-public class LadronPersonaje {
+public class LadronPersonaje extends Personaje {
+    private boolean invisible; // indicamos que el ladron esta en fase invisible
+
+    // Constructor
+    public LadronPersonaje(String nombre, int nivel, int puntosdeVida, Inventario inventario , boolean invisible) {
+        super(nombre, nivel, puntosdeVida, inventario);
+        this.invisible = false;
+    }
+    // Método para robar
+    public void robar() {
+        if (invisible) {
+            System.out.println("El ladrón ha robado sin que nadie lo viera");
+        } else {
+            System.out.println("El ladrón a intentado robar, pero el fue descubierto.");
+        }
+    }
+    // Método para hacerse invisible
+    public void hacerseInvisible() {
+        invisible = !invisible; // Cambiar el estado de la invisibilidad
+        if (invisible) {
+            System.out.println("El ladrón es invisible");
+        } else {
+            System.out.println("El ladrón ya no es invisible");
+        }
+    }
+    // Método para verificar si es el ladrón esta invisible
+    public boolean estaInvisible() {
+        return invisible;
+    }
 }
